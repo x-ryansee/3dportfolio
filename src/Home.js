7e1802f97bcc73c25894d4886174105e63fc1a8c
Ryan See <ryanjosephsee@gmail.com>
2023-04-25 15:03:41 -0700

    adding navbar

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import './styles.css';
 export default function Home() {
   return (
     <div className="spline-container">
-      <Spline scene="https://prod.spline.design/4dWs7HTM1eRpNVWv/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/4dWs7HTM1eRpNVWv/scene.splinecode" />
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__toggle" onClick={toggleNav}>
+        {showNav ? (
+          <FontAwesomeIcon icon={faTimes} />
+        ) : (
+          <FontAwesomeIcon icon={faBars} />
+        )}
+      </div>
+      <ul className={showNav ? 'navbar__list active' : 'navbar__list'}>
+        <li className="navbar__item">
+          <a href="#">Links</a>
+          <ul className="navbar__sublist">
+            <li>
+              <a href="#">LinkedIn</a>
+            </li>
+            <li>
+              <a href="#">Github</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar__item">
+          <a href="#">Projects</a>
+          <ul className="navbar__sublist">
+            <li>
+              <a href="#">Tahoe + Napa Idylls</a>
+            </li>
+            <li>
+              <a href="#">The Loop</a>
+            </li>
+            <li>
+              <a href="#">Fusion Kitchen</a>
+            </li>
+          </ul>
+        </li>
+        <li className="navbar__item">
+          <a href="#">About</a>
+          <ul className="navbar__sublist">
+            <li>
+              <a href="#">Resume</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
