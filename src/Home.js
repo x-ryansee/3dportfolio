3c74b22b902bb55687652125f4978e4c9cfa892c
Ryan See <ryanjosephsee@gmail.com>
2023-04-25 10:51:31 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+import './styles.css';
+
+export default function Home() {
+  return (
+    <div className="spline-container">
+      <Spline scene="https://prod.spline.design/4dWs7HTM1eRpNVWv/scene.splinecode" />
+    </div>
+  );
+}
