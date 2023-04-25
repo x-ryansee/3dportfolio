adc1bf09d1f76b4951047f4685594dfbc422d921
Ryan See <ryanjosephsee@gmail.com>
2023-04-25 15:50:39 -0700

    navbar 2/10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Home from './Home'
+import NavBar from './NavBar'
 import './App.css';
 
 function App() {
   return (
+    <div>
+      <NavBar />
       <Home />
+    </div>
   );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,48 +3,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
-const Navbar = () => {
+const NavBar = () => {
   const [showNav, setShowNav] = useState(false);
 
   const toggleNav = () => {
     setShowNav(!showNav);
   };
-
-  return (
-    <nav className="navbar">
-      <div className="navbar__toggle" onClick={toggleNav}>
-        {showNav ? (
-          <FontAwesomeIcon icon={faTimes} />
-        ) : (
-          <FontAwesomeIcon icon={faBars} />
-        )}
-      </div>
-      <ul className={showNav ? 'navbar__list active' : 'navbar__list'}>
-        <li className="navbar__item">
-          <a href="#">Links</a>
-          <ul className="navbar__sublist">
-            <li>
-              <a href="#">LinkedIn</a>
-            </li>
-            <li>
-              <a href="#">Github</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-          </ul>
-        </li>
-        <li className="navbar__item">
-          <a href="#">Projects</a>
-          <ul className="navbar__sublist">
-            <li>
-              <a href="#">Tahoe + Napa Idylls</a>
-            </li>
-            <li>
-              <a href="#">The Loop</a>
-            </li>
-            <li>
-              <a href="#">Fusion Kitchen</a>
+  
+    return (
+      <nav className="navbar">
+        <div className="navbar__toggle" onClick={toggleNav}>
+          {showNav ? (
+            <FontAwesomeIcon icon={faTimes} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </div>
+        <ul className={showNav ? 'navbar__list active' : 'navbar__list'}>
+          <li className="navbar__item">
+            <a href="#">Links</a>
+            <ul className="navbar__sublist">
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+              <li>
+                <a href="#">Github</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+          </li>
+          <li className="navbar__item">
+            <a href="#">Projects</a>
+            <ul className="navbar__sublist">
+              <li>
+                <a href="#">Tahoe + Napa Idylls</a>
+              </li>
+              <li>
+                <a href="#">The Loop</a>
+              </li>
+              <li>
+                <a href="#">Fusion Kitchen</a>
             </li>
           </ul>
         </li>
@@ -61,4 +61,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default NavBar;
